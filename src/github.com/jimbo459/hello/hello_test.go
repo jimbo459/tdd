38da3a9e4f10618f05b0dd9840a1150a05d66ad7
James Norman <jnorman@pivotal.io>
2020-01-17 10:35:22 +0000

    Working hello world with var

--- src/github.com/jimbo459/hello/hello_test.go
+++ src/github.com/jimbo459/hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello()
-	want := "Hello World!"
+	got := hello("James")
+	want := "Hello James"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
