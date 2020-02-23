9e93b5428beb9801a89f1fda20763a8a92edf914
James Norman <jnorman@pivotal.io>
2020-02-23 17:19:24 +0000

    select logic around returning servers

--- src/github.com/jimbo459/select/racer.go
+++ src/github.com/jimbo459/select/racer.go
@@ -1,22 +1,38 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) string {
-	startA := time.Now()
-	http.Get(a)
-	aTime := time.Since(startA)
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, time.Second*10)
+}
 
-	startB := time.Now()
-	http.Get(b)
-	bTime := time.Since(startB)
-
-	if aTime < bTime {
-		return a
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("Get timed out.")
 	}
+}
 
-	return b
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
 }
--- src/github.com/jimbo459/select/racer_test.go
+++ src/github.com/jimbo459/select/racer_test.go
@@ -8,25 +8,41 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second * 5)
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedWebServer(time.Second * 5)
+	fastServer := makeDelayedWebServer(time.Second * 1)
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second * 1)
-		w.WriteHeader(http.StatusOK)
-	}))
+	defer slowServer.Close()
+	defer fastServer.Close()
 
 	slowUrl := slowServer.URL
 	fastUrl := fastServer.URL
 
 	want := fastUrl
 
-	got := Racer(slowUrl, fastUrl)
+	got, _ := Racer(slowUrl, fastUrl)
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
 
+	t.Run("returns an error if Server does not respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedWebServer(time.Second * 11)
+		serverB := makeDelayedWebServer(time.Second * 12)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("Expected error!")
+		}
+	})
+}
+
+func makeDelayedWebServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
